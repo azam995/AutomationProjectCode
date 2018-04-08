3b6ae7b566c598cb28f67c23f6edc2dcbc01610b
Ekramul <Ekramul@EkramulAzam.home>
2018-04-08 17:17:00 -0400

    One more update

--- Ditmas/src/test/java/com/Brooklyn/Ditmas/Application.java
+++ Ditmas/src/test/java/com/Brooklyn/Ditmas/Application.java
@@ -14,5 +14,13 @@ public class Application {
 		driver.get("https://www.facebook.com/");
 	
 	}
-
+ 
+	@Test
+	public void mytest1020()
+	{
+		System.setProperty("webdriver.chrome.driver","C:/Users/Ekramul/Documents/Selenium file folder/SeleniumWebdriver/chromedriver_win32/chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.yahoo.com/");
+	
+	}
 }
