f18cf35c6e154a3bdbaf4af954cb0f556aa0e597
Ekramul <Ekramul@EkramulAzam.home>
2018-04-08 17:12:38 -0400

    New another project

---
+++ Ditmas/src/main/java/com/Brooklyn/Ditmas/App.java
@@ -0,0 +1,13 @@
+package com.Brooklyn.Ditmas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Ditmas/src/test/java/com/Brooklyn/Ditmas/Application.java
@@ -0,0 +1,18 @@
+package com.Brooklyn.Ditmas;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Application {
+	
+	@Test
+	public void mytest102()
+	{
+		System.setProperty("webdriver.chrome.driver","C:/Users/Ekramul/Documents/Selenium file folder/SeleniumWebdriver/chromedriver_win32/chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+	
+	}
+
+}
